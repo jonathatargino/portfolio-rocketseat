3bd9f13c9c180d65449f60b8389a7f8cb76d8dd9
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-01-12 21:19:32 -0300

    App now contains SideBar and UserProjects components only

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import SideBar from './components/SideBar'
+import UserProjects from './components/UserProjects'
 
 function App() {
 
   return (
-    <>
+    <div className="mainDiv">
       <SideBar />
-    </>
+      <UserProjects/>
+    </div>
   )
 }
 
